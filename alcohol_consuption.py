065dff66a54f9414f224e4bd3754e15e34a55282
0regor <vinicius-pd@hotmail.com>
2019-04-28 00:00:06 -0300

    Adicionado % população urbana, inicio de codificação, apenas uma pequena divisão em relação ao modelo de dados

--- alcohol_consuption.py
+++ alcohol_consuption.py
@@ -8,6 +8,22 @@ from sklearn.cluster import KMeans
 import pandas as pd
 
 # Carga da base de dados a ser utilizada
-data = pd.read_csv("drinks.csv")
+# [0] country    ; [1] beer_servings ; [2] spirit_servings ; [3] wine_servings      ; [4] total_litres_of_pure_alcohol ;
+# [5] continente ; [6] região        ; [7] população       ; [8] população urbana(%);
+data = pd.read_excel("drinks.xlsx")
 
-print(data.head())
+# Comando para verificação das 5 primeiras linhas da base de dados
+#print(data.head())
+
+#Define os atributos utilizados
+X = data.iloc[:, :5]
+
+#Define o possível target
+y = data['continente']
+
+#Define nº clusters baseado no número de registros únicos do target
+num_clusters = len(set(y))
+
+#print(X.head())
+print(num_clusters)
+
